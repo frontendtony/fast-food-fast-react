976208e78f733491fc578fa2fb18d99b48461020
Anthony Oyathelemhi <tonero91@gmail.com>
2018-11-12 03:21:39 +0100

    convert CartFooter to a class component and add checkout to the button click

--- src/components/CartFooter.js
+++ src/components/CartFooter.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CartFooter = (props) => {
-  const { total, addCommaToNum, emptyCart } = props;
-  return (
-    <div className="cart-footer">
-      <input 
-          className="raised address input" type="text" 
-          placeholder="Enter optional delivery address" />
-      <div className="cart-checkout-group">
-        <h6 className="total amount">Total: {"\u20A6"} 
-          <span id="total"> {addCommaToNum(total)} </span>
-        </h6>
-        <div className="horizontal button-group">
-          <button className="raised large checkout button complete">
-            Checkout
-          </button>
-          <button 
-            className="raised inverted large button decline"
-            onClick={emptyCart}>
-            Empty Cart
-          </button>
+class CartFooter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { address: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      address: e.target.value
+    });
+  }
+
+  render() {
+    const { cart, total, addCommaToNum, emptyCart, checkout } = this.props;
+    const { address } = this.state;
+    return (
+      <div className="cart-footer">
+        <input 
+          className="raised address input" type="text"
+          placeholder="Enter optional delivery address"
+          onChange={this.handleChange}
+        />
+        <div className="cart-checkout-group">
+          <h6 className="total amount">Total: {"\u20A6"} 
+            <span id="total"> {addCommaToNum(total)} </span>
+          </h6>
+          <div className="horizontal button-group">
+            <button 
+              className="raised large checkout button complete"
+              onClick={() => checkout(cart, address)}>
+              Checkout
+            </button>
+            <button 
+              className="raised inverted large button decline"
+              onClick={emptyCart}>
+              Empty Cart
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CartFooter;
