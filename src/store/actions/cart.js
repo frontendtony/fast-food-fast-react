9fb0ae052ccdd198fe5283264fb96f3e0d67a053
Anthony Oyathelemhi <tonero91@gmail.com>
2018-11-12 03:17:54 +0100

    create checkout action creator and map to EMPTY_CART reducer

--- src/store/actions/cart.js
+++ src/store/actions/cart.js
@@ -1,11 +1,14 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from '../actionTypes';
+import { addError } from './errors';
+import { apiCall } from '../../services/api';
+import _ from 'lodash';
 
 export const addToCart = (foodObject) => {
   return (dispatch) => {
     dispatch({
       type: ADD_TO_CART,
-      payload: foodObject,
-    })
+      payload: foodObject
+    });
   };
 }
 
@@ -14,14 +17,47 @@ export const removeFromCart = (id) => {
     dispatch({
       type: REMOVE_FROM_CART,
       payload: id
-    })
+    });
   }
 }
 
 export const emptyCart = () => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({
       type: EMPTY_CART
+    });
+  }
+}
+
+export const checkout = (cart, address = '') => {
+  return (dispatch) => {
+    const foodIds = _.keys(cart);
+    const data = { foodIds, address };
+    const { token } = localStorage;
+    if (!token) return addError('You need to be logged in to place an order');
+    const config = {
+      url: `/orders`,
+      method: 'post',
+      headers: { 'Authorization': `Bearer ${token}` },
+      data
+    }
+    
+    return new Promise((resolve, reject) => {
+      const request = apiCall(config);
+      request.then(() => {
+        dispatch(emptyCart());
+        resolve();
+      })
+      .catch((err) => {
+        const { response, request, message } = err;
+        const error = response ? response.data.message
+          : request ? 'Network error, please try again later' : message;
+        dispatch(addError(error));
+      })
     })
   }
 }
+
+// dispatch({
+//   type: CHECKOUT
+// });
