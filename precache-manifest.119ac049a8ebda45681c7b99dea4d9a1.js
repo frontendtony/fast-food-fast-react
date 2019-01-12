54113ccbc91a655d80a6da2d91b9d20f84a1e1aa
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-12 01:53:39 +0100

    Updates

---
+++ precache-manifest.119ac049a8ebda45681c7b99dea4d9a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7aa557f552384f5f734",
+    "url": "/fast-food-fast-react/static/js/runtime~main.f7aa557f.js"
+  },
+  {
+    "revision": "ad7093518e3d9e3df296",
+    "url": "/fast-food-fast-react/static/js/main.ad709351.chunk.js"
+  },
+  {
+    "revision": "30052b35e310bfc60107",
+    "url": "/fast-food-fast-react/static/js/1.30052b35.chunk.js"
+  },
+  {
+    "revision": "ad7093518e3d9e3df296",
+    "url": "/fast-food-fast-react/static/css/main.fb3e703c.chunk.css"
+  },
+  {
+    "revision": "aebc2e5b1464748a4fbcfcf6116e3c8d",
+    "url": "/fast-food-fast-react/index.html"
+  }
+];
